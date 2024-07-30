8fbdd5cea827fe5d5a09b4e87cbbe59fc3832f19
kllerredstark <redstars1022@gmail.com>
2024-07-30 09:58:32 +0900

    work

---
+++ src/main/java/com/ll/flllly/FllllyApplication.java
@@ -0,0 +1,13 @@
+package com.ll.flllly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FllllyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FllllyApplication.class, args);
+	}
+
+}
